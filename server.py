85eefabd3a7bef3a30e057d0b5751d28db054b93
Xin <1071165895@qq.com>
2020-06-06 10:38:46 +0800

    fix

--- server.py
+++ server.py
@@ -17,7 +17,7 @@ Content-Type: text/html
 
 '''
 f = open('index.html', 'r')
-index_content = header + f.read()
+index_content = index_content + f.read()
 f.close()
 
 # client.js
@@ -73,6 +73,59 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((HOST, PORT))
 s.listen(5)
 
+# while True:
+#     try:
+#         conn, addr = s.accept()
+#         request = conn.recv(1024)
+#         data = request.decode().split(' ')
+#         # request = request.decode().split(' ')
+#         if len(data) > 1:
+#             method = data[0]
+#             src = data[1]
+#             # print(conn)
+#             print(addr[0]+':'+str(addr[1])+'-->'+src)
+#             # print(request.decode())
+#         else:
+#             continue
+#
+#         if method == 'GET':
+#             if src == '/index.html':
+#                 f = open('index.html', "rb")
+#                 conn.send("HTTP/1.0 200 OK\r\n".encode())
+#                 conn.send("Content-Type:text/html\r\n".encode())
+#                 conn.send("\r\n".encode())
+#                 while 1:
+#                     data = f.read(1024)
+#                     if not data:
+#                         break
+#                     conn.send(data)
+#                 f.close()
+#             elif src == '/client.js':
+#                 f = open('client.js', "rb")
+#                 conn.send("HTTP/1.0 200 OK\r\n".encode())
+#                 conn.send("Content-Type:text/javascript\r\n".encode())
+#                 conn.send("\r\n".encode())
+#                 while 1:
+#                     data = f.read(1024)
+#                     if not data:
+#                         break
+#                     conn.send(data)
+#                 f.close()
+#             elif src == '/background.png':
+#                 conn.sendall(background_content)
+#             elif src == '/blackStone.png':
+#                 conn.sendall(blackStone_content)
+#             elif src == '/whiteStone.png':
+#                 conn.sendall(whiteStone_content)
+#             elif src == '/favicon.ico':
+#                 conn.sendall(favicon_content)
+#             else:
+#                 header = 'HTTP/1.1 404 NOT FOUND\r\n\r\n'
+#                 conn.send(header.encode())
+#         conn.close()
+#     except KeyboardInterrupt:
+#         break
+
 while True:
     try:
         conn, addr = s.accept()
@@ -89,25 +142,40 @@ while True:
             continue
 
         if method == 'GET':
-            if src == '/index.html':
-                print("src == /index.html")
-                conn.sendall(index_content.encode(encoding='UTF-8'))
-                print("send index finish")
-            elif src == '/client.js':
-                conn.sendall(js_content.encode(encoding='UTF-8'))
-            elif src == '/background.png':
-                conn.sendall(background_content)
-            elif src == '/blackStone.png':
-                conn.sendall(blackStone_content)
-            elif src == '/whiteStone.png':
-                conn.sendall(whiteStone_content)
-            elif src == '/favicon.ico':
-                conn.sendall(favicon_content)
-            else:
-                header = 'HTTP/1.1 404 NOT FOUND\r\n\r\n'
-                conn.send(header.encode())
-        conn.close()
+            try:
+                if src[-4:] == ".png" or src[-4:] == ".ico":
+                    f = open(src[1:], 'rb')
+                    conn.send("HTTP/1.0 200 OK\r\n".encode())
+                    conn.send("Content-Type:image/{}\r\n".format(src[-3:]).encode())
+                    conn.send("\r\n".encode())
+                    while 1:
+                        data = f.read(1024)
+                        if not data:
+                            break
+                        conn.send(data)
+                    f.close()
+                elif src[-5:] == ".html" or src[-3:] == ".js":
+                    f = open(src[1:], "r")
+                    outputstr = f.readlines()
+                    outputdata = [line.encode() for line in outputstr]
+                    conn.send("HTTP/1.0 200 OK\r\n".encode())
+                    if src[-3:] == ".js":
+                        content_type = "Content-Type:text/javascript\r\n"
+                    else:
+                        content_type = "Content-Type:text/html\r\n"
+                    conn.send(content_type.encode())
+                    conn.send("\r\n".encode())
+                    for data in outputdata:
+                        conn.send(data)
+                    f.close()
+            except FileNotFoundError:
+                print("file not found")
+                conn.send("HTTP/1.0 404 Not Found\r\n".encode())
+                continue
+            finally:
+                conn.close()
     except KeyboardInterrupt:
         break
-
+s.shutdown(2)
+s.close()
 
